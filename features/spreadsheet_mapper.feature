d01676c5d155a2503fbd93d4b76c77267ff7aa41
Charlie Quinn <charlesquinn1984@gmail.com>
2011-12-02 17:05:40 +0000

    cuke the lotta them

---
+++ features/spreadsheet_mapper.feature
@@ -0,0 +1,7 @@
+Feature: Map file contents to models
+
+@wip
+Scenario: csv file
+  Given something cool
+  When something cool happens
+  Then something cool should be here
---
+++ features/step_definitions/spreadsheet_mapper_steps.rb
@@ -0,0 +1,11 @@
+Given /^something cool$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^something cool happens$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^something cool should be here$/ do
+  pending # express the regexp above with the code you wish you had
+end
