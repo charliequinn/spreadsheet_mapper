ab918ab3139b56de6bbaf54ffd42bebd90bc7697
Charlie Quinn <charlesquinn1984@gmail.com>
2011-12-02 16:46:00 +0000

    rspec added

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'spreadsheet_mapper'
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
---
+++ spec/spreadsheet_mapper_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe SpreadsheetMapper do
+  pending "write it"
+end
